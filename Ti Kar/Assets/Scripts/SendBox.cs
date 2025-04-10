64cd39b56b02fd839553b8391f69dae4f35aa049
JosueRFP <josue.petri.senac@gmail.com>
2025-04-10 11:38:39 -0300

    Novo objetivo do jogo entrega de caixas

---
+++ Ti Kar/Assets/Scripts/SendBox.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SendBox : MonoBehaviour
+{
+    [SerializeField] GameObject box;
+    public Transform boxPosition;
+    
+    private float nextToDrop;
+
+    void Update()
+    {          
+        OnMouseDown();
+    }
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(box, mousePosition, Quaternion.identity);
+        }
+        
+    }
+     
+    
+}
