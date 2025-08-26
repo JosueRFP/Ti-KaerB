4c583890d913e37acf528a29fe24d2641a3b5db2
JosueRFP <josue.petri.senac@gmail.com>
2025-08-26 11:23:04 -0300

    Camera pode "andar"

--- Ti Kar/Assets/Scripts/MoveCamera.cs
+++ Ti Kar/Assets/Scripts/MoveCamera.cs
@@ -4,15 +4,20 @@ using UnityEngine.AI;
 public class MoveCamera : MonoBehaviour
 {
     NavMeshAgent agent;
+
+    [SerializeField] Transform[] flyPoints; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(agent. remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(flyPoints[Random.Range(0, flyPoints.Length)].position);
+        }
     }
 }
