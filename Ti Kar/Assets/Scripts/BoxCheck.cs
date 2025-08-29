76b730024b2b88a80c994bbcc4ab2e8c567b11f6
JosueRFP <josue.petri.senac@gmail.com>
2025-08-29 10:47:44 -0300

    Construçoes para o ambiente.

---
+++ Ti Kar/Assets/Scripts/BoxCheck.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class BoxCheck : MonoBehaviour
+{
+    [SerializeField] public int allMails;
+    [SerializeField] TextMeshPro mailsTxt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
